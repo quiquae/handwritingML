71afe6ce597d524b3e8725853427d8e116bb31ba
quiquae <54282044+quiquae@users.noreply.github.com>
2019-09-22 14:26:59 +0100

    Add files via upload

--- mnistconverter.py
+++ mnistconverter.py
@@ -1,3 +1,4 @@
+# every single thing i have to import for this image edit code (my goD!)
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from scipy.ndimage import gaussian_filter
@@ -6,9 +7,15 @@ from skimage import color
 from skimage import io
 from skimage.measure import block_reduce
 from skimage.transform import rescale
+from skimage import img_as_ubyte
+import cv2
 
 blurredness = 2 # controls how blurry gaussian filter is
 greyleniency = 16 # how many tones of leniency for considering something absolute white for bounding box, i.e. 3 --> anything >252 is white, <3 is black
+
+#-----------------------------------------------------------------------
+#----------------------------- BASIC FUNCTIONS------------------------
+#-----------------------------------------------------------------------
 
 # plots + shows image
 def showimg(image):
@@ -16,6 +23,7 @@ def showimg(image):
     plt.show()
 
 # crops out white segments of up to the amount permitted by greyleniency
+# theres probably a library with a special function for this but i decided to code myself :)
 def boundingbox(image):
     # variables showing indexes for the rows/column bounds
     left_bound = 0
@@ -84,10 +92,10 @@ def boundingbox(image):
                 allwhite = False
 
     # prints new bounds
-    print("Right bound: "+str(right_bound))
-    print("Left bound: "+str(left_bound))
-    print("Upper bound: "+str(upper_bound))
-    print("Lower bound: "+str(lower_bound))
+##    print("Right bound: "+str(right_bound))
+##    print("Left bound: "+str(left_bound))
+##    print("Upper bound: "+str(upper_bound))
+##    print("Lower bound: "+str(lower_bound))
     
     # creates new, smaller array according to the new bounds
     new_array = []
@@ -103,16 +111,24 @@ def insquare(image,addp):
     # find height + width
     height = len(image[0])
     width = len(image)
+##    print("width: ", width)
+##    print("height: ", height)
     # what each value from original image array needs to be offset by to center it in the new square array
     offsetheight = addp
     offsetwidth = addp
     # take the larger value
     if(height>width):
         sqlength = height+2*addp
-        offsetwidth = int((height-width)/2+0.5)+addp
+        offsetwidth = int((height-width)/2+0.5)
+    elif(height<width): #CHANGED THIS ELSE->ELIF (EXCLUDE EQUALITY i.e. square CASE)
+        sqlength = width+2*addp
+        offsetheight += int((width-height)/2+0.5) #removed "+addp" as using += and already set offsetheight as addp!! REMEMBER
+##        print(offsetheight)
+##        print("ENTERRED HERE1")
     else:
         sqlength = width+2*addp
-        offsetheight += int((width-height)/2+0.5)+addp
+##        print("ENTERRED HERE2")
+
     # initialize it to 255 (white)
     new_array = np.full((sqlength,sqlength),255)
     # add values from image array into new array offset by the offset values
@@ -122,38 +138,95 @@ def insquare(image,addp):
     # returns new centered square image array
     return(new_array)
 
-# path on computer where image is stored
-data_path = "C:\\Users\\creag\\OneDrive\\compsci-proj\\test_image.png"
+#---------------------------------------------------------------------
+#-------------------- COMPLEX/AGGREGATE FUNCTIONS---------------------
+#---------------------------------------------------------------------
 
-# converts to greyscale not rgb photo
-test_image = color.rgb2gray(io.imread(data_path))
-print("Greyscale image loaded.")
-showimg(test_image)
+def openimageasbinary(data_path):
+    # loads photo + converts to greyscale (not rgb photo!!!) then converts to binary
+    image = cv2.cvtColor(io.imread(data_path),cv2.COLOR_BGR2GRAY)
+    # uses weird threshold thing- DO NOT ALTER OR WILL NOT WORK:
+    #cv2 threshold for the 0-255 greyscale value to turn to black/white depending on value "minimizing intra-class intensity variance"  (otsu)
+    z,binary_image = cv2.threshold(image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    return(binary_image)
 
-# blurs with gaussian filter
-blurred_image = gaussian_filter(test_image,sigma=blurredness)
-print("Gaussian filter applied.")
-showimg(blurred_image)
+def gaussian(image, blur):
+    # gaussian blur- yay!
+    return(gaussian_filter(image, blur))
 
-# cuts whitespace to put image in bounding box
-bounded_image = boundingbox(blurred_image)
-print("Image cropped. ")
-showimg(bounded_image)
+def pixelsquare(image):
+    # fits bounding box to image
+    bounded_image = boundingbox(image)
+    # puts inside square
+    squared_image = insquare(bounded_image,0)
+    # pixelates/rescales it to 20x20
+    rescaling_factor = 20/len(squared_image)
+    rescaled_image = rescale(np.asarray(bounded_image), rescaling_factor, anti_aliasing=False,multichannel=False)
+    rescaled_image = img_as_ubyte(rescaled_image)
+    # pads 20x20 square inside 28x28 whitespace
+    padded_image = insquare(rescaled_image, 4)
+    return(padded_image)
 
-# centers bounding box inside square
-squared_image = insquare(bounded_image,0)
-print("Image squared.")
+# opens image and converts it to MNIST format!
+# datapath = where image is stored, blur = gaussian blur/sigma, showimage = whether image is plotted during function
+def mnistconvert(datapath, blur, showimage):
+    # loads orignally rgb image to binary + adds gaussian blur
+    image = gaussian(openimageasbinary(datapath),blur)
+    # puts inside pixelated square
+    image = pixelsquare(image)
+    # shows depending on the value of bool showimage
+    if(showimage):
+        showimg(image)
+    return(image)
 
-# rescales image to 20x20 pixel box, 8-bit resolution
-rescaling_factor = 20/len(squared_image)
-# rescaling factor is what the length of arrays are multiplied by to make smaller
-rescaled_image = rescale(np.asarray(bounded_image), rescaling_factor, anti_aliasing=False,multichannel=False)
-print("Image rescaled.")
-showimg(rescaled_image)
+#----------------------------------------------------------------------------------------
+#---------------------------- USAGE EXAMPLE ---------------------------------------------
+#----------------------------------------------------------------------------------------
 
-# centers new image in 28x28 box
-# already in 20x20 pixel box so the added factor for padding is 4 (1/2 of 8) 
-padded_image = insquare(rescaled_image, 4)
-print("Image centered + padded.")
-showimg(padded_image)
-
+### path on computer where image is stored
+##data_path = "C:\\Users\\creag\\OneDrive\\compsci-proj\\actual_image2.jpeg"
+##
+### loads photo + converts to greyscale not rgb photo!!!
+##test_image = cv2.cvtColor(io.imread(data_path),cv2.COLOR_BGR2GRAY)
+##print("Greyscale image loaded.")
+##showimg(test_image)
+##
+### converts to binary
+##print("Image converted to binary.")
+##
+##    # uses weird threshold thing- DO NOT ALTER OR WILL NOT WORK:
+##    #cv2 threshold for the 0-255 greyscale value to turn to black/white depending on value "minimizing intra-class intensity variance"  (otsu)
+##z,binary_image = cv2.threshold(test_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+##showimg(binary_image)
+##
+### blurs with gaussian filter
+##blurred_image = gaussian_filter(binary_image,sigma=blurredness)
+##print("Gaussian filter applied.")
+##showimg(blurred_image)
+##
+### cuts whitespace to put image in bounding box
+##bounded_image = boundingbox(blurred_image)
+##print("Image cropped. ")
+##showimg(bounded_image)
+##
+### centers bounding box inside square
+##squared_image = insquare(bounded_image,0)
+##print("Image squared.")
+##showimg(squared_image)
+##
+### rescales image to 20x20 pixel box, 8-bit resolution
+##rescaling_factor = 20/len(squared_image)
+### rescaling factor is what the length of arrays are multiplied by to make smaller
+##rescaled_image = rescale(np.asarray(bounded_image), rescaling_factor, anti_aliasing=False,multichannel=False)
+##print("Image rescaled.")
+##showimg(rescaled_image)
+### converts from float to ubyte (integer 0-255) image
+##rescaled_image = img_as_ubyte(rescaled_image)
+##
+### centers new image in 28x28 box
+### already in 20x20 pixel box so the added factor for padding is 4 (1/2 of 8) 
+##padded_image = insquare(rescaled_image, 4)
+##print("Image centered + padded.")
+##showimg(padded_image)
+##
+##
