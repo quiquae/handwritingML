a355e12a0a640ae35169a68b67772fcf9bfaeffc
quiquae <creaghfactor@gmail.com>
2019-10-06 14:43:50 +0100

    this is my commitmessage?

--- neuralnetwork.py
+++
@@ -1,8 +0,0 @@
-# yes is 1, undecided is 0, no is -1
-# data (example) shows the votes of first 7 members of senate alphabetically for bills 393-397
-data = open("dataset.txt","r")
-datas = data.read().split(";;")
-for i in range(len(datas)):
-    dataset.append(datas[i].split("/"))
-
-print(dataset)
